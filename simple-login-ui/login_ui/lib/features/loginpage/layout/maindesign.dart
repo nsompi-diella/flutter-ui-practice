25aa0ea759c4c217d2863445732a2d9ce118c554
NSOMPI-DIELLA <nsompi.diella.lovinci@gmail.com>
2026-07-20 21:29:28 +0100

    feat(login-layout) : Update maindesign

---
+++ simple-login-ui/login_ui/lib/features/loginpage/layout/maindesign.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Maindesign extends StatefulWidget {
+  const Maindesign({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _Home();
+  }
+}
+
+class _Home extends State<Maindesign> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold();
+  }
+}
