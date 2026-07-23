e7638b68abbd0ab338d1255d86ca9e137fecdffc
NSOMPI-DIELLA <nsompi.diella.lovinci@gmail.com>
2026-07-23 03:44:14 +0100

    feat(signup-layout): Update maindesign_signup

--- simple-login-ui/login_ui/lib/features/signuppage/layout/maindesign_signup.dart
+++ simple-login-ui/login_ui/lib/features/signuppage/layout/maindesign_signup.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:login_ui/features/loginpage/widgets/submitted_email_button.dart';
 import 'package:login_ui/features/loginpage/widgets/text_barre_google.dart';
 import 'package:login_ui/features/signuppage/widgets/signupform.dart';
+import 'package:login_ui/features/signuppage/widgets/title_signup.dart';
 
 class MaindesignSignup extends StatefulWidget {
   const MaindesignSignup({super.key});
@@ -19,10 +20,12 @@ class _Home extends State<MaindesignSignup> {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Container(
-            margin: EdgeInsets.only(top: 100, left: 40),
+            margin: EdgeInsets.only(top: 100, left: 40, right: 10),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                TitleSignup(),
+                SizedBox(height: 20),
                 Signupform(),
                 SizedBox(height: 30),
                 TextBarreGoogle(),
