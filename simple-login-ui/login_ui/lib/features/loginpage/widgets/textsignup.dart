a16fc6446b248202a37854abed3e050ac581c9fc
NSOMPI-DIELLA <nsompi.diella.lovinci@gmail.com>
2026-07-23 03:02:00 +0100

    feat(login-widgets):Add textsignup file

---
+++ simple-login-ui/login_ui/lib/features/loginpage/widgets/textsignup.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Textsignup extends StatelessWidget {
+  const Textsignup({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(onPressed: () {}, child: Text('Create account'));
+  }
+}
