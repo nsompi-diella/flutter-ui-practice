cd1eed862202fdabb9320e98883bcd918ab4c64b
NSOMPI-DIELLA <nsompi.diella.lovinci@gmail.com>
2026-07-25 00:57:29 +0100

    core(controller): Add controller

---
+++ simple-login-ui/login_ui/lib/core/controller/controller.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Controller {
+  final usernameCtrl = TextEditingController();
+  final emailCtrl = TextEditingController();
+  final passwordCtrl = TextEditingController();
+}
