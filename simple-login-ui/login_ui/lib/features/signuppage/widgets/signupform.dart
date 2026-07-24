3879a3d3459deeccfe99683e33b6934fc41a5597
NSOMPI-DIELLA <nsompi.diella.lovinci@gmail.com>
2026-07-25 00:18:22 +0100

    feat(signup-widgets): Update signupform

--- simple-login-ui/login_ui/lib/features/signuppage/widgets/signupform.dart
+++ simple-login-ui/login_ui/lib/features/signuppage/widgets/signupform.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_ui/core/validation/validationform.dart';
 
 class Signupform extends StatefulWidget {
   const Signupform({super.key});
@@ -15,13 +16,96 @@ class _Home extends State<Signupform> {
     return SafeArea(
       child: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.only(),
+          padding: EdgeInsets.only(left: 10, right: 15),
           child: Column(
             children: [
               TextFormField(
+                validator: Validationform.email,
+                cursorColor: Colors.black,
+
                 decoration: InputDecoration(
+                  suffixIcon: Padding(
+                    padding: EdgeInsets.only(right: 30),
+                    child: Icon(Icons.person_outline_outlined),
+                  ),
+                  hint: Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Text(
+                      'Enter Username',
+                      style: TextStyle(color: Colors.grey, fontSize: 13),
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              TextFormField(
+                validator: Validationform.email,
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                  suffixIcon: Padding(
+                    padding: EdgeInsets.only(right: 30),
+                    child: Icon(Icons.email_outlined),
+                  ),
+                  hint: Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Text(
+                      'Enter email',
+                      style: TextStyle(color: Colors.grey, fontSize: 13),
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              TextFormField(
+                validator: Validationform.email,
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                  suffixIcon: Padding(
+                    padding: EdgeInsets.only(right: 30),
+                    child: Icon(Icons.lock_outline),
+                  ),
+                  hint: Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Text(
+                      'Enter password',
+                      style: TextStyle(color: Colors.grey, fontSize: 13),
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: Colors.grey),
                   ),
                 ),
               ),
