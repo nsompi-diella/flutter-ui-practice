dc3c67e4849652bb5fba2ee2d061e05c86357dd2
NSOMPI-DIELLA <nsompi.diella.lovinci@gmail.com>
2026-07-27 00:58:05 +0100

    layout :Add navigationbar to maindesign

--- powerapp_ui/powerapp/lib/layout/maindesign.dart
+++ powerapp_ui/powerapp/lib/layout/maindesign.dart
@@ -8,8 +8,13 @@ class Maindesign extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue.shade900,
-        leading: Padding(padding: EdgeInsets.all(8.0),child:
-        Image.asset('assets/image/official_ecg_logo.jpg',fit:BoxFit.contain,),),
+        leading: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Image.asset(
+            'assets/image/official_ecg_logo.jpg',
+            fit: BoxFit.contain,
+          ),
+        ),
         // title of the page
         title: Text('Home', style: TextStyle(color: Colors.white)),
         actions: [
@@ -32,6 +37,31 @@ class Maindesign extends StatelessWidget {
               label: Text('Get Help'),
               icon: Icon(Icons.info_outline),
             ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        // add type to fixe icon and label
+        type: BottomNavigationBarType.fixed,
+        unselectedItemColor: Colors.grey,
+        // put color of a selected icon
+        selectedItemColor: Colors.blue.shade900,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.grid_view_outlined),
+            label: 'Meter',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart_outlined),
+            label: 'Transaction',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet_outlined),
+            label: 'Wallets',
           ),
         ],
       ),
