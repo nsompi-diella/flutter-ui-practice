ab2b3851e79ecba48f32b5357af02678fc6ca891
NSOMPI-DIELLA <nsompi.diella.lovinci@gmail.com>
2026-07-26 23:32:10 +0100

    Update structure of the project

---
+++ powerapp_ui/powerapp/lib/layout/maindesign.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Maindesign extends StatelessWidget {
+  const Maindesign({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
