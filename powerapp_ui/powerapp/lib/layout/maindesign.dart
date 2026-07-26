c8b56a83402107ee2b7095d1892864a955eb683f
NSOMPI-DIELLA <nsompi.diella.lovinci@gmail.com>
2026-07-27 00:00:10 +0100

    layout : Add app bar to maindesign

--- powerapp_ui/powerapp/lib/layout/maindesign.dart
+++ powerapp_ui/powerapp/lib/layout/maindesign.dart
@@ -5,6 +5,34 @@ class Maindesign extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Home', style: TextStyle(color: Colors.white)),
+        actions: [
+          IconButton(
+            iconSize: 25,
+            onPressed: () {},
+            icon: Icon(Icons.notifications_outlined, color: Colors.white),
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 15),
+            child: ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.blue.shade900,
+              ),
+              onPressed: () {},
+              label: Text('Get Help'),
+              icon: Icon(Icons.info_outline),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
